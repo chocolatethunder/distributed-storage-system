2fddc3df65972b5119b14c821150c90346ff0147
Itachi0970 <dkennygold@hotmail.com>
2019-03-12 11:36:06 -0600

    Updated relative path for test file

--- jcp/src/main/java/app/App.java
+++ jcp/src/main/java/app/App.java
@@ -3,11 +3,16 @@
  */
 package app;
 
+import java.io.*;
+
 public class App {
 
     public static void main(String[] args) {
 
         RequestSender requestSender = RequestSender.getInstance();
+		
+		//File file = new File("test.txt");
+		//String path = file.getParent();
 
         requestSender.sendFile("test.txt");
 
