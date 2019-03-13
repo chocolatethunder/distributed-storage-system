1e48273ccb8b54008e0b3cdd87a23cc3edc35e10
B <bryanmcgregor02@gmail.com>
2019-03-12 22:11:42 -0600

    added more functionality to indexer

--- stalker/src/main/java/app/Indexer.java
+++ stalker/src/main/java/app/Indexer.java
@@ -10,9 +10,10 @@ import java.util.Optional;
 import java.nio.charset.StandardCharsets;
 
 //This class will be in control of the indexFiles
+//all these functions are probably going to need to be thread safe
 public class Indexer {
 
-
+    //loads the json from file and converts it to an IndexFile object
     public static IndexFile loadFromFile(String indexPath){
         ObjectMapper mapper = new ObjectMapper();
         Optional<IndexFile> ind = Optional.empty();
@@ -25,17 +26,17 @@ public class Indexer {
         return ind.get();
     }
 
+    //prints the indexfile object to file
     public static boolean writeIndex(IndexFile ind){
         try {
+            ObjectMapper mapper = new ObjectMapper();
+            //Object to JSON in String
+            String jsonInString = mapper.writeValueAsString(ind);
 
         }
-        catch(){
-            
+        catch(IOException e){
+            e.printStackTrace();
         }
-        ObjectMapper mapper = new ObjectMapper();
-        //Object to JSON in String
-        String jsonInString = mapper.writeValueAsString(ind);
-
         return true;
     }
 
