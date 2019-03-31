1b21288446d854ba21e85bc86b1b7a71311f737f
Masroor Hussain Syed <suroor.23@gmail.com>
2019-03-31 13:23:38 -0600

    Update UdpPacket.java

--- library/src/main/java/app/UdpPacket.java
+++ library/src/main/java/app/UdpPacket.java
@@ -1,28 +1,44 @@
 package app;
 
-import java.io.Serializable;
-import java.net.InetAddress;
+public class UDPPacket {
 
-public class UdpPacket {
-	private String type;
-	private String target;
-	private InetAddress address;
-	
-	public UdpPacket(RequestType requestType, String target, InetAddress address){
+    private String type;
+    private String uuid;
+    private String target;
+    private String address;
+
+    public UDPPacket(MessageType requestType, String uuid, String target, String address){
 
         this.type = requestType.name();
+        this.uuid = uuid;
         this.target =  target;
         this.address = address;
     }
-	
-	public String getType() {
-		return this.type;
-	}
-	public String getTarget() {
-		return this.target;
-	}
-	public InetAddress getAddress() {
-		return this.address;
-	}
+    public String getType() {
+        return type;
+    }
+    public void setType(String type) {
+        this.type = type;
+    }
+    public String getUuid() {
+        return uuid;
+    }
+    public void setUuid(String uuid) {
+        this.uuid = uuid;
+    }
+    public String getTarget() {
+        return target;
+    }
+    public void setTarget(String target) {
+        this.target = target;
+    }
+    public String getAddress() {
+        return address;
+    }
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+
 
 }
