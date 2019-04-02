2b21eb96e69f087385001d9a4bb3225d901e4ac7
RedRaven <bryanmcgregor02@gmail.com>
2019-04-02 00:28:12 -0600

    Update NetDiscovery.java

--- library/src/main/java/app/NetDiscovery.java
+++ library/src/main/java/app/NetDiscovery.java
@@ -96,7 +96,6 @@ public class NetDiscovery implements Runnable{
             InetAddress replyAddress =  InetAddress.getByName(discoverReply.get("address").textValue());
             stalkerMap.put(Integer.valueOf(uuid), replyAddress.getHostAddress());
         }
-        receiverSocket.close();
         socket.close();
         return stalkerMap;
     }
