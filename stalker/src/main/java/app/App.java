8faedd885406da75c31b7032c81c9eba92b218d5
RedRaven <bryanmcgregor02@gmail.com>
2019-04-11 06:16:23 -0600

    mayb not

--- stalker/src/main/java/app/App.java
+++ stalker/src/main/java/app/App.java
@@ -122,7 +122,8 @@ public class App {
             cfg.setLeader_id(leaderUuid);
             ConfigManager.saveToFile(cfg);
         }
-
+        Thread elecListen2 = new Thread(new ElectionListener());
+        elecListen2.start();
         while (true){
             //reelect
             //starting task for health checks on STALKERS and HARM targets
