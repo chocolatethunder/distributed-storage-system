16be0107d3728346f443812da283bc8ee079632d
RedRaven <bryanmcgregor02@gmail.com>
2019-04-12 23:27:16 -0600

    debug

--- stalker/src/main/java/app/App.java
+++ stalker/src/main/java/app/App.java
@@ -275,7 +275,7 @@ public class App {
                     wait(5000);
                 }
             } catch (IOException e) {
-                Debugger.log("Stalker main: Attempt failed when connecting to leader...", null);
+                Debugger.log("Stalker main: Attempt failed when connecting to leader...", e);
                 wait(5000);
                 Debugger.log("Stalker main: Trying again...", null);
 
